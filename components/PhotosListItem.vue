d4b7da2c6768089251867fc6729ac598f461157d
Arnelle Balane <arnellebalane@gmail.com>
2018-05-28 13:35:03 +0800

    Define PhotosListItem component

---
+++ components/PhotosListItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="photos-list-item">
+        <img :src="data.placeholder" alt="Photo Item" class="placeholder">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PhotosListItem',
+
+        props: {
+            data: {
+                type: Object,
+                required: true
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .photos-list-item {
+        position: relative;
+    }
+
+    img {
+        display: block;
+        width: 100%;
+    }
+</style>
