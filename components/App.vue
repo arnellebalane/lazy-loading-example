7c3aae07cf42b387a87deb653be5fed9c50fb5ba
Arnelle Balane <arnellebalane@gmail.com>
2018-05-28 12:57:47 +0800

    Define PhotosList component

--- components/App.vue
+++ components/App.vue
@@ -1,5 +1,18 @@
 <template>
     <div id="app">
         <h1>Lazy Loading Example</h1>
+        <PhotosList />
     </div>
 </template>
+
+<script>
+    import PhotosList from './PhotosList.vue';
+
+    export default {
+        name: 'App',
+
+        components: {
+            PhotosList
+        }
+    };
+</script>
---
+++ components/PhotosList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="photos-list">
+
+    </div>
+</template>
+
+<script>
+    import { mapState, mapActions } from 'vuex';
+
+    export default {
+        name: 'PhotosList',
+
+        computed: mapState(['photos']),
+
+        created() {
+            this.fetchPhotos();
+        },
+
+        methods: mapActions(['fetchPhotos'])
+    };
+</script>
